04b93e620d8c0acceb2d502d8c896d066fceb4b1
Robert Lyn <lenny4422@gmail.com>
2022-07-13 17:34:37 -0400

    Add movement and jumping

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -2,12 +2,22 @@ extends KinematicBody2D
 
 var gravity = 300
 var velocity = Vector2.ZERO
+var maxHorizontalSpeed = 100
+var jumpSpeed = 200
 
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	velocity.y += gravity * delta;
+	var moveVector = Vector2.ZERO
+	moveVector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	velocity.x = moveVector.x * maxHorizontalSpeed
+		
+	moveVector.y = -1 if Input.is_action_just_pressed("jump") else 0
 	
-	velocity = move_and_slide(velocity)
+	if (moveVector.y < 0 && is_on_floor()):
+		velocity.y = moveVector.y * jumpSpeed
+	
+	velocity.y += gravity * delta;
+	velocity = move_and_slide(velocity, Vector2.UP)
